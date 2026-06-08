49cf541c8f1038f7c8fb3932d92b8ac314758e81
LoverITer <2489868503@qq.com>
2026-06-08 12:38:39 +0800

    fix

--- src/features/workbench/pages/AppWorkbenchPage.tsx
+++ src/features/workbench/pages/AppWorkbenchPage.tsx
@@ -109,11 +109,11 @@ export function AppWorkbenchPage() {
         </div>
 
         {/* 移动端：Tab 切换布局 */}
-        <div className="flex h-full flex-col md:hidden">
+        <div className="flex h-full min-h-0 flex-col md:hidden">
           <Tabs
             activeKey={activeTab}
             onChange={(key) => setActiveTab(key as 'chat' | 'preview')}
-            className="flex-1 min-h-0 [&_.ant-tabs-nav]:mb-0 [&_.ant-tabs-nav]:px-3 [&_.ant-tabs-tab]:px-4! [&_.ant-tabs-tab]:py-2.5! [&_.ant-tabs-content-holder]:!flex [&_.ant-tabs-content-holder]:!min-h-0 [&_.ant-tabs-content]:!h-full [&_.ant-tabs-tabpane]:!h-full"
+            className="flex! min-h-0! flex-col! [&_.ant-tabs-nav]:mb-0 [&_.ant-tabs-nav]:shrink-0 [&_.ant-tabs-nav]:px-3 [&_.ant-tabs-tab]:px-4! [&_.ant-tabs-tab]:py-2.5! [&_.ant-tabs-content-holder]:flex! [&_.ant-tabs-content-holder]:min-h-0! [&_.ant-tabs-content-holder]:flex-1! [&_.ant-tabs-content]:h-full! [&_.ant-tabs-tabpane]:h-full!"
             items={[
               {
                 key: 'chat',
